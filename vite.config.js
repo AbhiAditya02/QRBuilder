c96b7ca2522e88335ce5ef815e53e8cc5ff4808f
Abhi Aditya <abhiaditya0755@gmail.com>
2026-01-18 14:12:08 +0530

    feat: configure build options in vite.config.js for optimized production

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,32 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+
+  build: {
+    sourcemap: false,
+
+    minify: 'terser',
+
+    terserOptions: {
+      compress: {
+        drop_console: true,  
+        drop_debugger: true,  
+      },
+      mangle: true,          
+      format: {
+        comments: false,      
+      },
+    },
+
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
